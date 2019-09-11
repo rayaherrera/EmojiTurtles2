2e4cdf7aae388a81443c147bceda8fff26501642
Ray Herrera <100158301@lubbockisd.net>
2019-09-11 08:58:53 -0500

    Initial commit

---
+++ EmojiTurtle.py
@@ -0,0 +1,21 @@
+import turtle
+ray1=turtle
+ray1.bgcolor("black")
+ray1.color("yellow")
+ray1.begin_fill()
+ray1.circle(120)
+ray1.end_fill()
+
+import turtle
+ray2=turtle
+ray2.color("black")
+ray2.penup()
+ray2.left(90)
+ray2.forward(150)
+ray2.left(90)
+ray2.forward(30)
+ray2.pendown()
+ray2.forward(50)
+turtle.exitonclick()
+
+
