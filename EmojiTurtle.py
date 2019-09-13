681d9bdfef66348e9459cb8a8da239bda701f546
rayaherrera <rayaherrera@aol.com>
2019-09-13 08:32:15 -0500

    .

--- EmojiTurtle.py
+++ EmojiTurtle.py
@@ -16,6 +16,29 @@ ray2.left(90)
 ray2.forward(30)
 ray2.pendown()
 ray2.forward(50)
-turtle.exitonclick()
 
+import turtle
+ray3=turtle
+ray3.color("blue")
+ray3.penup()
+ray3.left(180)
+ray3.forward(90)
+ray3.right(90)
+ray3.pendown()
+ray3.begin_fill()
+ray3.circle(25)
+ray3.end_fill()
+
+import turtle
+ray4=turtle
+ray4.color("deep pink")
+ray4.penup()
+ray4.forward(100)
+ray4.left(90)
+ray4.forward(25)
+ray4.right(180)
+ray4.pendown()
+ray4.forward(70)
+
+turtle.exitonclick()
 
---
+++ README.md
@@ -0,0 +1,2 @@
+My emoji is a subtle winking face. One turtle makes the face, another makes the flat eye, another makes the circular eye, and the last on makes the flat mouth. I made the turtles all different colors so that it would be easy to recognize all four turtles.
+[GitHub Logo](/EmojiTurtle.py)
